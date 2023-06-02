9d98e4ceb0be9db053525cfe4d440979dbe5d28e
Nir Jacobson <nirjacobson@gmail.com>
2023-06-02 14:14:03 -0400

    remove files

--- src/internal/fps_panel.h
+++
@@ -1,8 +0,0 @@
-#ifndef FPS_PANEL_INTERNAL_H
-#define FPS_PANEL_INTERNAL_H
-
-#include "../fps_panel.h"
-
-void fps_panel_draw(void* fpsPanelPtr);
-
-#endif // FPS_PANEL_INTERNAL_H
--- src/internal/picker.h
+++
@@ -1,8 +0,0 @@
-#ifndef PICKER_INTERNAL_H
-#define PICKER_INTERNAL_H
-
-#include "../picker.h"
-
-void picker_act(Picker* picker, char modifier1, char modifier2);
-
-#endif // PICKER_INTERNAL_H
